8353e41bbff04c23279ef539620c567c05f74a99
adaramirez <adalu.gypsy@gmail.com>
2018-07-30 22:43:33 -0500

    second attempt, the creation of the functionality was successful

--- js/app.js
+++ js/app.js
@@ -1,33 +1,54 @@
-var dropTarget = document.querySelector('.source'); //esto es el contenedor donde están las imagenes
-var draggables = document.querySelectorAll('.yellow'); //llama a todas las imagenes
-
-console.log(dropTarget);
-console.log(draggables);
-
-
-for (let i = 0; i < draggables.length; i++) {
-    draggables[i].addEventListener('dragstart', function (ev) {
-        ev.dataTransfer.setData('srcId', ev.target.id);
-    });
+function allowDrop(event) {
+    event.preventDefault();
 }
 
 
-dropTarget.addEventListener('dragover', function (ev) {
-    ev.preventDefault();
-});
+function drag(event) {
+    event.dataTransfer.setData('text', event.target.id);
+    $(event.target.id).parent().remove();
+}
 
-dropTarget.addEventListener('drop', function (ev) {
-    ev.preventDefault();
-    let target = ev.target;
-    let droppable = target.classList.contains('container');
-    let srcId = ev.dataTransfer.getData('srcId');
 
-    if (droppable) {
-        ev.target.appendChild(document.getElementById(srcId));
-    }
+function drop(event) {
+    event.preventDefault();
+    var data = event.dataTransfer.getData('text');
+    event.target.appendChild(document.getElementById(data));
+    console.log(drop);
+}
 
-    console.log(srcId);
-});
+
+
+
+// var dropTarget = document.querySelector('.source'); //esto es el contenedor donde están las imagenes
+// var draggables = document.querySelectorAll('.yellow'); //llama a todas las imagenes
+
+// console.log(dropTarget);
+// console.log(draggables);
+
+
+// for (let i = 0; i < draggables.length; i++) {
+//     draggables[i].addEventListener('dragstart', function (ev) {
+//         ev.dataTransfer.setData('srcId', ev.target.id);
+//     });
+// }
+
+
+// dropTarget.addEventListener('dragover', function (ev) {
+//     ev.preventDefault();
+// });
+
+// dropTarget.addEventListener('drop', function (ev) {
+//     ev.preventDefault();
+//     let target = ev.target;
+//     let droppable = target.classList.contains('container');
+//     let srcId = ev.dataTransfer.getData('srcId');
+
+//     if (droppable) {
+//         ev.target.appendChild(document.getElementById(srcId));
+//     }
+
+//     console.log(srcId);
+// });
 
 
 
